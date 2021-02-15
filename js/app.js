d673c8029bb67d76cd8ec8683effe1adc5bcd3dd
jhl73 <silmido27javier@gmail.com>
2021-02-15 08:19:27 -0300

    agregando estilos css y js parte 2

--- js/app.js
+++ js/app.js
@@ -1,4 +1,26 @@
 const seccionesPagina = new fullpage('#fullpage', {
-    
 
+//OPCIONES BASICAS
+autoScrolling: true,
+fitToSection: false,
+fitToSectionDelay: 300,
+easing: 'easeInOutCubic',
+scrollingSpeed: 700,
+css3: true,
+easingcss3: 'ease-out',
+loopBottom: false,
+
+//BARRA DE NAVEGACION
+navigation: true,
+menu: '#menu',
+anchors: ['Inicio', 'Productos', 'Contacto'],
+navigationTooltips: ['Inicio', 'Productos', 'Contacto'],
+showActiveTooltip: false,
+
+
+//SECCIONES
+sectionsColor : ['#000', '#c2c2c2', '#000'],
+verticalCentered: true,
 });
+
+
