7cf7dd260e31a5d4264160c3d618c754f050c650
jhl73 <silmido27javier@gmail.com>
2021-02-15 23:30:49 -0300

    final fullpage demo

--- js/app.js
+++ js/app.js
@@ -21,6 +21,17 @@ showActiveTooltip: false,
 //SECCIONES
 sectionsColor : ['#000', '#c2c2c2', '#000'],
 verticalCentered: true,
+
+
+//SLIDER
+
+controlArrows: true,
+slidesNavigation: false,
+afterLoad: function(origin, destination){
+    if(destination.anchor == 'contacto'){
+        document.querySelector('.footer h2').style.opacity = 1;
+    }
+}
+
+
 });
-
-
